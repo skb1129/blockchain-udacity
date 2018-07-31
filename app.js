569557b5dda1994c25ef5447b59baac49d60c33b
Surya Kant Bansal <skb1129@yahoo.com>
2018-07-31 17:48:14 +0530

    refactor: eslint rules

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Blockchain, Block } = require('./simpleChain');
+
 const app = express();
 const port = 8000;
 
-const { Blockchain, Block } = require('./simpleChain');
 const blockchain = new Blockchain();
 
 app.use(bodyParser.json());
@@ -11,11 +12,11 @@ app.use(bodyParser.json());
 app.get('/block/:blockHeight', async (req, res) => {
   const block = await blockchain.getBlock(req.params.blockHeight);
   res.json(block);
-})
+});
 
 app.post('/block', async (req, res) => {
   const block = await blockchain.addBlock(new Block(req.body.blockData));
   res.json(block);
-})
+});
 
 app.listen(port, () => console.log(`Blockchain service running on port: ${port}`));
--- simpleChain.js
+++ simpleChain.js
@@ -1,41 +1,42 @@
-/* ===== SHA256 with Crypto-js ===============================
-|  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
-|  =========================================================*/
-
+/**
+ * SHA256 with Crypto-js
+ * Learn more: Crypto-js: https://github.com/brix/crypto-js
+ */
 const SHA256 = require('crypto-js/sha256');
 const level = require('level');
+
 const chainDB = './chaindata';
 const db = level(chainDB);
 
-/* ===== Block Class ==============================
-|  Class with a constructor for block 			   |
-|  ===============================================*/
-
-class Block{
-  constructor(data){
-    this.hash = "",
-    this.height = 0,
-    this.body = data,
-    this.time = 0,
-    this.previousBlockHash = ""
+/**
+ * Block Class
+ * Class with a constructor for block
+ */
+class Block {
+  constructor(data) {
+    this.hash = '';
+    this.height = 0;
+    this.body = data;
+    this.time = 0;
+    this.previousBlockHash = '';
   }
 }
 
-/* ===== Blockchain Class ==========================
-|  Class with a constructor for new blockchain 		|
-|  ================================================*/
-
-class Blockchain{
-  constructor(){
+/**
+ * Blockchain Class
+ * Class with a constructor for new blockchain
+ */
+class Blockchain {
+  constructor() {
     this.initBlockchain();
   }
 
-  async initBlockchain(){
+  async initBlockchain() {
     try {
       await db.get(0);
     } catch (error) {
       if (error.notFound) {
-        this.addBlock(new Block("First block in the chain - Genesis block"));
+        this.addBlock(new Block('First block in the chain - Genesis block'));
       } else {
         console.log('Error getting data from DB', error);
       }
@@ -43,15 +44,17 @@ class Blockchain{
   }
 
   // Add new block
-  async addBlock(newBlock){
+  async addBlock(newBlock) {
     let chainHeight = 0;
-    await this.getBlockHeight().then(value => chainHeight = value);
+    await this.getBlockHeight().then((value) => {
+      chainHeight = value;
+    });
     if (chainHeight >= 0) {
       const prevBlock = await this.getBlock(chainHeight);
       newBlock.previousBlockHash = prevBlock.hash;
     }
     newBlock.height = chainHeight + 1;
-    newBlock.time = new Date().getTime().toString().slice(0,-3);
+    newBlock.time = new Date().getTime().toString().slice(0, -3);
     newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     const block = JSON.stringify(newBlock);
     try {
@@ -64,13 +67,13 @@ class Blockchain{
   }
 
   // Get block height
-  async getBlockHeight(){
+  async getBlockHeight() {
     return new Promise((resolve, reject) => {
       let chainHeight = 0;
-      db.createReadStream().on('data', (data) => {
-        chainHeight++;
+      db.createReadStream().on('data', () => {
+        chainHeight += 1;
       }).on('error', (error) => {
-        console.log('Unable to read data stream!', error)
+        console.log('Unable to read data stream!', error);
         reject();
       }).on('close', () => {
         resolve(chainHeight - 1);
@@ -79,7 +82,7 @@ class Blockchain{
   }
 
   // get block
-  async getBlock(blockHeight){
+  async getBlock(blockHeight) {
     try {
       const data = await db.get(blockHeight);
       return JSON.parse(data);
@@ -90,25 +93,26 @@ class Blockchain{
   }
 
   // validate block
-  validateBlock(block){
+  validateBlock(block) {
     const testBlock = { ...block, hash: '' };
     const blockHash = block.hash;
     const validBlockHash = SHA256(JSON.stringify(testBlock)).toString();
-    if (blockHash===validBlockHash) {
+    if (blockHash === validBlockHash) {
       return true;
-    } else {
-      console.log(`Block #${block.height} invalid hash:\n${blockHash}<>${validBlockHash}`);
-      return false;
     }
+    console.log(`Block #${block.height} invalid hash:\n${blockHash}<>${validBlockHash}`);
+    return false;
   }
 
   // Validate blockchain
-  async validateChain(){
-    let errorLog = [];
+  async validateChain() {
+    const errorLog = [];
     let chainHeight = 0;
-    await this.getBlockHeight().then(value => chainHeight = value);
+    await this.getBlockHeight().then((value) => {
+      chainHeight = value;
+    });
 
-    for(let i = 0; i <= chainHeight; i++){
+    for (let i = 0; i <= chainHeight; i += 1) {
       const block = await this.getBlock(i);
       console.log('Validating Block: ', block.height);
       const isValid = this.validateBlock(block);
@@ -119,12 +123,12 @@ class Blockchain{
         const prevBlock = await this.getBlock(i - 1);
         const { previousBlockHash } = block;
         const { hash } = prevBlock;
-        if ( previousBlockHash !== hash) {
+        if (previousBlockHash !== hash) {
           errorLog.push(prevBlock.height);
         }
       }
 
-      if (i === chainHeight && errorLog.length>0) {
+      if (i === chainHeight && errorLog.length > 0) {
         console.log(`Block errors = ${errorLog.length}`);
         console.log(`Blocks: ${errorLog}`);
       } else if (i === chainHeight) {
